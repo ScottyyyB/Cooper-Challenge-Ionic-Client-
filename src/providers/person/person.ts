818849efda7c6cc32783b4355096d32ff92e976a
ScottB <17sb@cis.dk>
2017-12-14 14:35:17 +0100

    Modify person.ts, and update home.ts

--- src/providers/person/person.ts
+++ src/providers/person/person.ts
@@ -9,11 +9,9 @@ export class PersonProvider {
   public age: number;
   public assessmentMessage: string;
 
-  constructor(private cooper: CooperProvider) {
+  constructor(private cooper: CooperProvider) {}
 
-    doAssessment(distance: number): void {
-      this.assessmentMessage = this.cooper.assess(this, distance);
-    }
+  doAssessment(distance: number): void {
+    this.assessmentMessage = this.cooper.assess(this, distance);
   }
-
 }
