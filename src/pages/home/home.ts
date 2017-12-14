be8bfc48b9a452b75b01712347cfcd3358f93311
ScottB <17sb@cis.dk>
2017-12-14 15:12:24 +0100

    Add cooper assessment message display and modify person gender first letter to be capital

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,9 +16,13 @@ export class HomePage {
     this.user = { distance: 1000, age: 20, gender: 'female' };
   }
 
+  firstLetterCapitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
   calculate() {
     this.person.age = this.user.age;
-    this.person.gender = this.user.gender;
+    this.person.gender = this.firstLetterCapitalize(this.user.gender);
 
     this.person.doAssessment(this.user.distance);
     console.log(this.person.assessmentMessage);
